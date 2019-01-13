6e68797c45ae8d69f5042e056b12813cfd8a5fb2
AI Enthusiast <18482571+pchao6@users.noreply.github.com>
2019-01-13 16:05:06 +0800

    Update 021. Merge Two Sorted Lists.py

--- 0. P001-P100/021. Merge Two Sorted Lists.py
+++ 0. P001-P100/021. Merge Two Sorted Lists.py
@@ -11,14 +11,14 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        dummy_head = tail = ListNode(0)
+        dummy_head = head = ListNode(0)
 
         while l1 and l2:
             if l1.val < l2.val:
-                tail.next, l1 = l1, l1.next
+                head.next, l1 = l1, l1.next
             else:
-                tail.next, l2 = l2, l2.next
-            tail = tail.next
+                head.next, l2 = l2, l2.next
+            head = head.next
 
-        tail.next = l1 or l2
+        head.next = l1 or l2
         return dummy_head.next
