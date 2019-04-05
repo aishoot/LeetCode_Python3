2c7f59790812ea9951ff223999c46d57c901d456
AI Enthusiast <18482571+pchao6@users.noreply.github.com>
2019-04-05 22:36:41 +0800

    101. Symmetric Tree

---
+++ 01. P101-P200/101. Symmetric Tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def isMirror(t1, t2):
+            if t1 == None and t2 == None:
+                return True
+            if t1 == None or t2 == None:
+                return False
+            return t1.val == t2.val and isMirror(t1.right, t2.left) and isMirror(t1.left, t2.right)
+
+        return isMirror(root, root)
