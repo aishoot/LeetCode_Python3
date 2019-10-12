3af62d88c5202225b144ef3a6e55bf8d29a1d52c
AI Enthusiast <18482571+pchao6@users.noreply.github.com>
2019-10-12 18:13:16 +0800

    344. Reverse String

---
+++ 03. P301-P400/344. Reverse String.py
@@ -0,0 +1,9 @@
+# 一行代码解决问题
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+
+
